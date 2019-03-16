91c50ae47a7d19139961783e2183f46fb0949afa
coder3101 <ashar786khan@gmail.com>
2019-03-16 20:27:34 +0530

    compatibility with new yap_matrix for testing

--- main.cc
+++ main.cc
@@ -14,22 +14,18 @@
  * limitations under the License.
  */
 
-#include "benchmark.hpp"
-#include "include/matrix.hpp"
-#include "normal_matrix.hpp"
 #include <cassert>
 #include <chrono>
+#include "benchmark.hpp"
+#include "include/yap_matrix.hpp"
+#include "normal_matrix.hpp"
 
 /**
- * @brief A Lambda that returns the lazy_matrix with [row,col] and filled with v
+ * @brief A Lambda that returns the yap_matrix with [row,col] and filled with v
  *
  */
-auto get_lazy_matrix = [](size_t row, size_t col, int v) -> test::matrix_int {
-  std::vector<std::vector<int>> data{row, std::vector<int>(col)};
-  for (size_t t = 0; t < row; t++)
-    for (size_t j = 0; j < col; j++)
-      data[t][j] = v;
-  return test::matrix_int(data);
+auto get_lazy_matrix = [](size_t row, size_t col, int v) {
+  return test::yap::matrix(std::vector<int>(row * col, v), row, col);
 };
 
 /**
@@ -41,8 +37,7 @@ auto get_normal_matrix = [](size_t row, size_t col,
                             int v) -> test::Matrix<int> {
   std::vector<std::vector<int>> data{row, std::vector<int>(col)};
   for (size_t t = 0; t < row; t++)
-    for (size_t j = 0; j < col; j++)
-      data[t][j] = v;
+    for (size_t j = 0; j < col; j++) data[t][j] = v;
   return test::Matrix<int>(data);
 };
 
@@ -50,63 +45,52 @@ auto get_normal_matrix = [](size_t row, size_t col,
  * @brief A Generic Lambda that computes a very long operation.
  *
  */
-auto compute_something = [](auto &target, auto &scope) {
-  target =
-      scope + scope * scope + scope / scope + scope + scope + scope + scope +
-      scope + scope + scope + scope / scope + scope + scope + scope + scope -
-      scope + scope + scope + scope / scope + scope + scope + scope + scope +
-      scope + scope + scope + scope / scope + scope + scope + scope + scope -
-      scope + scope + scope + scope / scope + scope + scope + scope + scope +
-      scope + scope + scope + scope / scope + scope + scope + scope + scope -
-      scope + scope + scope + scope * scope + scope + scope + scope + scope +
-      scope + scope + scope + scope * scope + scope + scope + scope + scope -
-      scope + scope + scope + scope * scope + scope + scope + scope + scope +
-      scope + scope + scope + scope * scope + scope + scope + scope + scope -
-      scope + scope + scope + scope * scope + scope + scope + scope + scope +
-      scope + scope + scope + scope * scope + scope + scope + scope + scope -
-      scope + scope + scope + scope * scope + scope + scope + scope + scope +
-      scope + scope + scope + scope * scope + scope + scope + scope + scope -
-      scope + scope + scope + scope * scope + scope + scope + scope + scope +
-      scope + scope + scope + scope * scope + scope + scope + scope + scope -
-      scope + scope + scope + scope * scope + scope + scope + scope + scope -
-      scope + scope + scope + scope * scope + scope + scope + scope + scope -
-      scope + scope + scope + scope * scope + scope + scope + scope + scope +
-      scope + scope + scope + scope * scope + scope + scope + scope + scope -
-      scope + scope + scope + scope * scope + scope + scope + scope + scope -
-      scope + scope + scope;
+auto compute_something_naive = [](auto &target, auto &scope) {
+  target = scope + scope * scope + scope / scope + scope + scope + scope +
+           scope + scope + scope + scope + scope + scope + scope + scope +
+           scope + scope + scope / scope + scope + scope + scope + scope -
+           scope + scope + scope + scope / scope + scope + scope + scope +
+           scope + scope + scope + scope + scope / scope + scope + scope +
+           scope + scope - scope + scope + scope + scope / scope + scope +
+           scope + scope + scope + scope + scope + scope + scope / scope +
+           scope + scope + scope + scope - scope + scope + scope +
+           scope * scope + scope + scope + scope + scope;
+};
+
+auto compute_something_yap = [](auto &target, auto &scope) {
+  test::yap::assign(
+      target, scope + scope * scope + scope / scope + scope + scope + scope +
+                  scope + scope + scope + scope + scope + scope + scope +
+                  scope + scope + scope + scope / scope + scope + scope +
+                  scope + scope - scope + scope + scope + scope / scope +
+                  scope + scope + scope + scope + scope + scope + scope +
+                  scope / scope + scope + scope + scope + scope - scope +
+                  scope + scope + scope / scope + scope + scope + scope +
+                  scope + scope + scope + scope + scope / scope + scope +
+                  scope + scope + scope - scope + scope + scope +
+                  scope * scope + scope + scope + scope + scope);
 };
 
 int main() {
   using test::benchmark;
-  using test::matrix_int;
   using Matrix = test::Matrix<int>;
 
   // Block 1
   {
-    matrix_int a = get_lazy_matrix(1000, 1000, 0);
-    matrix_int b = get_lazy_matrix(1000, 1000, 10);
+    auto a = get_lazy_matrix(2000, 2000, 0);
+    auto b = get_lazy_matrix(2000, 2000, 10);
 
-    Matrix a2 = get_normal_matrix(1000, 1000, 0);
-    Matrix b2 = get_normal_matrix(1000, 1000, 10);
+    auto a2 = get_normal_matrix(2000, 2000, 0);
+    auto b2 = get_normal_matrix(2000, 2000, 10);
 
-    auto result1 = benchmark::run("Execution with Expression template",
-                                  [&]() { compute_something(a, b); });
+    auto result1 = benchmark::run("Execution using YAP Matrix",
+                                  [&]() { compute_something_yap(a, b); });
     auto result2 = benchmark::run("Execution without Expression template",
-                                  [&]() { compute_something(a2, b2); });
+                                  [&]() { compute_something_naive(a2, b2); });
     result2.print_beautifully();
     result1.print_beautifully();
-    assert(a2 == a);
-    assert(a == a);
-  }
-  // Block 2
-  {
-    matrix_int a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    matrix_int b = {{7, 8, 9}, {4, 5, 6}, {1, 2, 3}};
-    Matrix a2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-    Matrix b2 = {{7, 8, 9}, {4, 5, 6}, {1, 2, 3}};
-    auto result = Matrix::dot(a2, b2);
-    auto c = (a | b);
-    assert(result == c);
+    assert(result2 == result1);
+    fflush(stdout);
   }
 
   return 0;
